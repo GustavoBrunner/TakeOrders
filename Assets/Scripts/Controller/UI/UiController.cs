bceec69e19268f478011939e66c66f06a2e0b619
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2023-08-17 21:51:10 -0300

    17/08.2 programação

--- Assets/Scripts/Controller/UI/UiController.cs
+++ Assets/Scripts/Controller/UI/UiController.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using Controller.Commands;
 namespace Controller
 {
     public class UiController : MonoBehaviour
     {
         private static UiController _instance;
         public static UiController Instance { get => _instance; }
+        private List<Button> uiBtns;
+        private RectTransform uiTransform;
+        private int index;
+        private Button nextButton 
+        { 
+            get 
+            {
+                index++;
+                return uiBtns[index];
+            } 
+        }
 
         private void Awake()
         {
             CreateSingleton();
+            uiBtns = new List<Button>();
+            index = 0;
+        }
+        private void Start()
+        {
+            uiBtns.AddRange(FindObjectsOfType<Button>());
+            Debug.Log(uiBtns.Count);
+            uiTransform = GameObject.Find("Panel").GetComponent<RectTransform>();
+            CloseUi();
         }
 
+        private void Update()
+        {
+            if (InputState.SelectChoice1)
+            {
+                OpenUi();
+            }
+            if (InputState.SelectChoice2)
+            {
+                CloseUi();
+            }
+            if(InputState.DownChoiceBtn || InputState.DownChoiceBtn2)
+            {
+                ChangeChoiceBtn();
+            }
+            if (InputState.UpChoiceBtn || InputState.UpChoiceBtn2)
+            {
+                ChangeChoiceBtn();
+            }
+        }
         private void CreateSingleton()
         {
             if(_instance != null)
@@ -25,6 +65,35 @@ namespace Controller
                 _instance = this;
                 DontDestroyOnLoad(gameObject);
             }
+        }
+        private void OpenUi()
+        {
+            uiTransform.localScale = Vector2.one;
+            uiBtns[0].Select();
+        }
+        private void CloseUi()
+        {
+            uiTransform.localScale = Vector2.zero;
+        }
+        public void ChangeChoiceBtn()
+        {
+            if(index < uiBtns.Count)
+            {
+                nextButton?.Select();
+                
+            }
+        }
+        public void FirstOption()
+        {
+            Debug.Log("Primeira opção");
+        }
+        public void SecondOption()
+        {
+            Debug.Log("Segunda opção");
+        }
+        public void ThirdOption()
+        {
+            Debug.Log("Terceira opção");
         }
     }
 }
